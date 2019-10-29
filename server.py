fb7c14615a91a5361cbb00b3e8da38d9742a2d75
Sophie <szaniszlozs7@gmail.com>
2019-10-29 14:51:04 +0100

    started post answer function

--- server.py
+++ server.py
@@ -2,16 +2,32 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+saved_answer = {}
+
 
 @app.route('/')
 @app.route('/list')
 def route_list():
-    return render_template("index.html")
-    
+    answer_text = None
+    if 'answer' in saved_answer:
+        answer_text = saved_answer['answer']
+    return render_template("index.html", answer=answer_text)
+
+
+@app.route('/post-answer', methods=['POST'])
+def route_post():
+    if request.method == 'POST':
+        saved_answer['answer'] = request.form['answer']
+        return redirect('/list')
+    answer_text = None
+    if 'answer' in saved_answer:
+        answer_text = saved_answer['answer']
+    return render_template('edit.html', answer=answer_text)
+
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=8000,
+        port=7000,
         debug=True,
     )
